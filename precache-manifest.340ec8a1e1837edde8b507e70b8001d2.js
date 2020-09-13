19054f55755a5ef1e3efa8d03044bceb34eed212
varun5482 <varunmukherjee19@gmail.com>
2020-09-13 14:19:10 +0530

    Updates

---
+++ precache-manifest.340ec8a1e1837edde8b507e70b8001d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182f524417664d5aefe048720a97a9aa",
+    "url": "/navigatorApp/index.html"
+  },
+  {
+    "revision": "0144341891d5c5a6eee2",
+    "url": "/navigatorApp/static/css/main.147904a5.chunk.css"
+  },
+  {
+    "revision": "6746824352cb4b430baf",
+    "url": "/navigatorApp/static/js/2.69e7de1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/navigatorApp/static/js/2.69e7de1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0144341891d5c5a6eee2",
+    "url": "/navigatorApp/static/js/main.0f51aca4.chunk.js"
+  },
+  {
+    "revision": "22562bd8173051d70fc0",
+    "url": "/navigatorApp/static/js/runtime-main.2288559f.js"
+  }
+]);
